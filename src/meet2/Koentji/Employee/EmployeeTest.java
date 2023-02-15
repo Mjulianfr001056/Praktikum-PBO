9b1cf7513c4cfc3bf6982ea30e68683ee48d3f40
Mjulianfr001056 <222112217@stis.ac.id>
2023-02-16 00:19:48 +0700

    Delete Koentji

--- src/meet2/Koentji/Employee/EmployeeTest.java
+++
@@ -1,8 +0,0 @@
-package meet2.Koentji.Employee;
-
-public class EmployeeTest {
-    public static void main(String[] args) {
-        Employee p1 = new Employee(1, "Juan","Firdaus", 100);
-        
-    } 
-}
